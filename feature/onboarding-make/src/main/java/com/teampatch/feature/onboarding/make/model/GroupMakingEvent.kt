a41b9641c5f2742cb08bfc48a93ec2a9fb362738
agvber <agvber12@gmail.com>
2025-06-07 01:21:20 +0900

    chore: 사용하지 않는 모듈 제거

--- feature/onboarding-make/src/main/java/com/teampatch/feature/onboarding/make/model/GroupMakingEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.teampatch.feature.onboarding.make.model
-
-sealed interface GroupMakingEvent {
-    data object Success : GroupMakingEvent
-    data class Error(val t: Throwable) : GroupMakingEvent
-    data object Progress : GroupMakingEvent
-}
