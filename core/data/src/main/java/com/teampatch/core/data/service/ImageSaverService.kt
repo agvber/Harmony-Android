88a6ee3af3d7c22016be49f16cbae5dc4673e075
agvber <agvber12@gmail.com>
2025-06-17 14:28:00 +0900

    feat: 사용자 프로필 이미지 압축 및 저장 기능 추가

---
+++ core/data/src/main/java/com/teampatch/core/data/service/ImageSaverService.kt
@@ -0,0 +1,46 @@
+package com.teampatch.core.data.service
+
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import androidx.core.net.toUri
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Path
+import javax.inject.Inject
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.pathString
+
+class ImageSaverService @Inject constructor(
+    @ApplicationContext private val appContext: Context
+) {
+
+    fun saveProfileImage(inputStream: InputStream, fileFormat: String): Uri {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            val path: String = Path(appContext.dataDir.absolutePath, PROFILE_IMAGE_SUB_PATH)
+                .also { it.createDirectories() }
+                .pathString
+            return File(path).writeBytes(inputStream)
+        }
+
+        val profilePath: Path = Path(
+            appContext.dataDir.absolutePath,
+            PROFILE_IMAGE_SUB_PATH,
+            "$PROFILE_IMAGE_FILE_NAME.$fileFormat"
+        )
+            .also { it.createDirectories() }
+        return profilePath.toFile().writeBytes(inputStream)
+    }
+
+    private fun File.writeBytes(inputStream: InputStream): Uri {
+        return also { it.writeBytes(inputStream.readBytes()) }
+            .toUri()
+    }
+
+    companion object {
+        private const val PROFILE_IMAGE_SUB_PATH = "profile_image"
+        private const val PROFILE_IMAGE_FILE_NAME = "profileImage"
+    }
+}
