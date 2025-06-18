88d178b00c8541a67305eef6f0e6619368aecaea
agvber <agvber12@gmail.com>
2025-06-18 22:13:31 +0900

    feat: Coil 이미지 캐싱 비활성화 처리

--- app/src/main/java/com/teampatch/harmony/MainApplication.kt
+++ app/src/main/java/com/teampatch/harmony/MainApplication.kt
@@ -1,14 +1,24 @@
 package com.teampatch.harmony
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.request.CachePolicy
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application() {
+class MainApplication : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .diskCache(null)
+            .memoryCachePolicy(CachePolicy.DISABLED)
+            .build()
     }
 }
