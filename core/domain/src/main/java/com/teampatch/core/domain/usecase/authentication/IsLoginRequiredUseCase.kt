f3dc99dc5f0ef0d875c14b9c79c66bf72cecdd88
agvber <agvber12@gmail.com>
2025-06-19 19:34:15 +0900

    fix: Splash 화면 멈추는 문제 수정

--- core/domain/src/main/java/com/teampatch/core/domain/usecase/authentication/IsLoginRequiredUseCase.kt
+++ core/domain/src/main/java/com/teampatch/core/domain/usecase/authentication/IsLoginRequiredUseCase.kt
@@ -1,12 +1,12 @@
 package com.teampatch.core.domain.usecase.authentication
 
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class IsLoginRequiredUseCase @Inject constructor() {
 
-    operator fun invoke(): Flow<Boolean> {
-        return flowOf(false)
-    }
+    @OptIn(ObsoleteCoroutinesApi::class)
+    operator fun invoke(): Flow<Boolean> = flowOf(true)
 }
