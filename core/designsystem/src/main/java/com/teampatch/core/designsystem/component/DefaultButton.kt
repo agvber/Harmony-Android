06fdef80d3ccbd1afefc4752c3aa14add0163ba6
agvber <agvber12@gmail.com>
2025-06-21 14:39:21 +0900

    design: DefaultButton 디자인 확장성을 위해 shape, padding 인자 추가

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/component/DefaultButton.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/component/DefaultButton.kt
@@ -2,10 +2,12 @@ package com.teampatch.core.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LocalTextStyle
@@ -15,6 +17,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +35,8 @@ fun DefaultButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     color: DefaultButtonColor = DefaultButtonColor(),
+    shape: Shape = RoundedCornerShape(10.dp),
+    contentPaddingValues: PaddingValues = PaddingValues(),
     content: @Composable RowScope.() -> Unit,
 ) {
     Row(
@@ -42,8 +47,9 @@ fun DefaultButton(
             .heightIn(min = 68.dp)
             .background(
                 color = if (enabled) color.containerColor else color.disabledContainerColor,
-                shape = RoundedCornerShape(10.dp)
+                shape = shape
             )
+            .padding(contentPaddingValues)
             .noRippleClickable(enabled = enabled, onClick = onClick)
     ) {
         CompositionLocalProvider(
