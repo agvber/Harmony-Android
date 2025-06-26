1f0ad1a5f0057c58ef748347197aa234be1cc095
agvber <agvber12@gmail.com>
2025-06-26 13:18:46 +0900

    design: Common Layout Resource 추가

---
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/theme/Shape.kt
@@ -0,0 +1,6 @@
+package com.teampatch.core.designsystem.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val RoundedCornerShape10 = RoundedCornerShape(10.dp)
---
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/theme/TextSize.kt
@@ -0,0 +1,57 @@
+package com.teampatch.core.designsystem.theme
+
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+val SP1: TextUnit = 1.sp
+val SP2: TextUnit = 2.sp
+val SP3: TextUnit = 3.sp
+val SP4: TextUnit = 4.sp
+val SP5: TextUnit = 5.sp
+val SP6: TextUnit = 6.sp
+val SP7: TextUnit = 7.sp
+val SP8: TextUnit = 8.sp
+val SP9: TextUnit = 9.sp
+val SP10: TextUnit = 10.sp
+val SP11: TextUnit = 11.sp
+val SP12: TextUnit = 12.sp
+val SP13: TextUnit = 13.sp
+val SP14: TextUnit = 14.sp
+val SP15: TextUnit = 15.sp
+val SP16: TextUnit = 16.sp
+val SP17: TextUnit = 17.sp
+val SP18: TextUnit = 18.sp
+val SP19: TextUnit = 19.sp
+val SP20: TextUnit = 20.sp
+val SP21: TextUnit = 21.sp
+val SP22: TextUnit = 22.sp
+val SP23: TextUnit = 23.sp
+val SP24: TextUnit = 24.sp
+val SP25: TextUnit = 25.sp
+val SP26: TextUnit = 26.sp
+val SP27: TextUnit = 27.sp
+val SP28: TextUnit = 28.sp
+val SP29: TextUnit = 29.sp
+val SP30: TextUnit = 30.sp
+val SP31: TextUnit = 31.sp
+val SP32: TextUnit = 32.sp
+val SP33: TextUnit = 33.sp
+val SP34: TextUnit = 34.sp
+val SP35: TextUnit = 35.sp
+val SP36: TextUnit = 36.sp
+val SP37: TextUnit = 37.sp
+val SP38: TextUnit = 38.sp
+val SP39: TextUnit = 39.sp
+val SP40: TextUnit = 40.sp
+val SP41: TextUnit = 41.sp
+val SP42: TextUnit = 42.sp
+val SP43: TextUnit = 43.sp
+val SP44: TextUnit = 44.sp
+val SP45: TextUnit = 45.sp
+val SP46: TextUnit = 46.sp
+val SP47: TextUnit = 47.sp
+val SP48: TextUnit = 48.sp
+val SP49: TextUnit = 49.sp
+val SP50: TextUnit = 50.sp
+val SP51: TextUnit = 51.sp
+val SP52: TextUnit = 52.sp
