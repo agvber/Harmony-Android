fcb483bcd80db2ff64dc9f18bc92671fe3e0aeec
agvber <agvber12@gmail.com>
2025-06-26 13:33:54 +0900

    design: 자주 사용하는 TimePickerColor LocalProvider 선언

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/theme/Color.kt
@@ -1,5 +1,8 @@
 package com.teampatch.core.designsystem.theme
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TimePickerColors
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 val MainGreen = Color(0xFF00C573)
@@ -12,3 +15,19 @@ val G2 = Color(0xFFE6E3DD)
 val G3 = Color(0xFFC0BEB6)
 val G4 = Color(0xFF9B9B97)
 val G5 = Color(0xFF7A7971)
+
+@OptIn(ExperimentalMaterial3Api::class)
+val LocalHarmonyTimPickerColors = staticCompositionLocalOf<TimePickerColors> {
+    error("CompositionLocal LocalHarmonyTimPickerColors not present")
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+fun TimePickerColors.transferHarmonyColors(): TimePickerColors {
+    return copy(
+        containerColor = G1,
+        timeSelectorSelectedContainerColor = Green2,
+        timeSelectorUnselectedContainerColor = G2,
+        clockDialColor = G1,
+        periodSelectorSelectedContainerColor = G3
+    )
+}
--- core/designsystem/src/main/java/com/teampatch/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/theme/Theme.kt
@@ -1,8 +1,11 @@
 package com.teampatch.core.designsystem.theme
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val LightColorScheme = lightColorScheme(
     primary = MainGreen,
@@ -22,6 +25,7 @@ private val LightColorScheme = lightColorScheme(
      */
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HarmonyTheme(
     // Dynamic color is available on Android 12+
@@ -30,6 +34,12 @@ fun HarmonyTheme(
     MaterialTheme(
         colorScheme = LightColorScheme,
         typography = Typography,
-        content = content
+        content = {
+            CompositionLocalProvider(
+                LocalHarmonyTimPickerColors provides TimePickerDefaults.colors()
+                    .transferHarmonyColors(),
+                content = content
+            )
+        }
     )
 }
