68d0dd4ccf68e1fa8077cb06c9704cf9eafa5d45
agvber <agvber12@gmail.com>
2025-06-26 13:34:16 +0900

    design: Appbar 하단 divider 추가

--- core/designsystem/src/main/java/com/teampatch/core/designsystem/component/Appbar.kt
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/component/Appbar.kt
@@ -7,15 +7,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.teampatch.core.designsystem.theme.BL
+import com.teampatch.core.designsystem.theme.G3
+import com.teampatch.core.designsystem.theme.HarmonyTheme
 import com.teampatch.core.designsystem.theme.PretendardFontFamily
 
 @Composable
@@ -51,6 +56,24 @@ fun AppBar(
             horizontalArrangement = Arrangement.End,
             verticalAlignment = Alignment.CenterVertically,
             content = actions
+        )
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            thickness = 1.dp,
+            color = G3,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppbarPreview() {
+    HarmonyTheme {
+        AppBar(
+            title = { Text("설정") }
         )
     }
 }
