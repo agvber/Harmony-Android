339a623b365c63331d28b4b85f906b1142d5beb6
agvber <agvber12@gmail.com>
2025-06-30 11:50:30 +0900

    design: 공통 FloatingButton 추가

---
+++ core/designsystem/src/main/java/com/teampatch/core/designsystem/component/ItemFloatingButton.kt
@@ -0,0 +1,41 @@
+package com.teampatch.core.designsystem.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teampatch.core.designsystem.theme.HarmonyTheme
+import com.teampatch.core.designsystem.theme.MainGreen
+
+@Composable
+fun ItemFloatingButton(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .drawBehind {
+                drawCircle(color = MainGreen)
+            }
+            .size(64.dp)
+            .then(modifier)
+    ) {
+        content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ItemFloatingButtonPreview() {
+    HarmonyTheme {
+        ItemFloatingButton {
+            Text("+")
+        }
+    }
+}
