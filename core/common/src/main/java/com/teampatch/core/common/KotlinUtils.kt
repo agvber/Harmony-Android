96eaf66f0b79d407dcb632751b2666e8717e1379
agvber <agvber12@gmail.com>
2025-07-04 14:17:31 +0900

    feat: Iterable Set 변환 함수 추가

--- core/common/src/main/java/com/teampatch/core/common/KotlinUtils.kt
+++ core/common/src/main/java/com/teampatch/core/common/KotlinUtils.kt
@@ -3,3 +3,7 @@ package com.teampatch.core.common
 inline fun <T : Any?> T.takeIfNull(
     action: () -> Unit,
 ): Any = this ?: action()
+
+inline fun <T, R> Iterable<T>.set(transform: (T) -> R): Set<R> {
+    return mutableSetOf<R>().also { set -> forEach { set.add(transform(it)) } }
+}
