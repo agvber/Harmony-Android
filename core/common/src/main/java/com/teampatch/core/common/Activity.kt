7ac8691fcc7e23caa915fa9d41cef0a6c6b45f62
agvber <agvber12@gmail.com>
2025-07-04 10:40:15 +0900

    feat: toast stringResource를 받는 재사용 함수 추가

--- core/common/src/main/java/com/teampatch/core/common/Activity.kt
+++ core/common/src/main/java/com/teampatch/core/common/Activity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.provider.Settings
+import android.widget.Toast
+import androidx.annotation.StringRes
 
 fun Context.findActivity(): Activity? {
     var context = this
@@ -20,4 +22,12 @@ fun Context.startNotificationSettingsActivity(packageName: String) {
     val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
         .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
     startActivity(intent)
+}
+
+fun Toast.makeText(
+    context: Context,
+    @StringRes stringResource: Int,
+    duration: Int = Toast.LENGTH_SHORT
+): Toast {
+    return Toast.makeText(context, context.getString(stringResource), duration)
 }
